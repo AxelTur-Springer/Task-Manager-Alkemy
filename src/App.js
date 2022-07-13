2504ebcb45bbcc51f7969c684df96c5c50eebeb7
AxelTur-Springer <axel.turspringer@gmail.com>
2022-07-13 12:28:50 -0300

    Adding some simple routes react router @6

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { Login } from "./components/Login/Login";
 import { Register } from "./components/Register/Register";
 import { Error404 } from "./components/views/Error404/Error404";
 import { Tasks } from "./components/views/Task/Tasks";
+const RequireAuth = ({ children }) => {
+  if (!localStorage.getItem("logged")) {
+    return <Navigate to="/login" replace={true} />;
+  }
+  return children;
+};
+
 export const App = () => (
-  <>
-    <Routes>
-      <Route path="/" element={<Tasks />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/*" element={<Error404 />} />
-    </Routes>
-  </>
+  <Routes>
+    <Route
+      path="/"
+      element={
+        <RequireAuth>
+          <Tasks />
+        </RequireAuth>
+      }
+    />
+    <Route path="/login" element={<Login />} />
+    <Route path="/register" element={<Register />} />
+    <Route path="/*" element={<Error404 />} />
+  </Routes>
 );
