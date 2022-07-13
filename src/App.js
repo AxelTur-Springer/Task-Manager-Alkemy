f3c8576cf65be98b9475fcca20b328be8a2f94bd
AxelTur-Springer <axel.turspringer@gmail.com>
2022-07-13 10:48:04 -0300

    Adding a register component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
 import "./App.css";
 import { Login } from "./components/Login/Login";
-export const App = () => <Login />;
+import { Register } from "./components/Register/Register";
+export const App = () => (
+  <div className="container">
+    {/*<Register />*/}
+    <Login />
+  </div>
+);
