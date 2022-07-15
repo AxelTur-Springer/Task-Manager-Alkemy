7416d99a3837f05310d152e40c7af6b352678fd4
AxelTur-Springer <axel.turspringer@gmail.com>
2022-07-15 16:56:34 -0300

    created search bar using debounce

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,22 +1,59 @@
+import { useState } from "react";
 export const Card = ({
   data: {
     title,
     createdAt,
     user: { userName },
-    descripcion,
+    description,
     status,
     importance,
   },
 }) => {
+  const [showMore, SetShowMore] = useState(false);
+  const dateTime = new Date(createdAt).toLocaleString() + " hs.";
+
+  const limitString = (str) => {
+    if (str?.length > 370)
+      return { string: str.slice(0, 167).concat("..."), addButton: true };
+    return { string: str, addButton: false };
+  };
+
   return (
     <div className="card">
       <div className="close">x</div>
       <h3>{title}</h3>
-      <h6>{createdAt}</h6>
+      <h6>{dateTime}</h6>
       <h5>{userName}</h5>
-      <button type="button">{status.toLowerCase()}</button>
-      <button type="button">{importance.toLowerCase()}</button>
-      <p>{descripcion}</p>
+      <button className={status.toLowerCase()} type="button">
+        {status.toLowerCase()}
+      </button>
+      <button className={importance.toLowerCase()} type="button">
+        {importance.toLowerCase()}
+      </button>
+      {!showMore && <p>{limitString(description).string}</p>}
+      {showMore && (
+        <>
+          <p>{description}</p>{" "}
+          <button
+            type="button"
+            onClick={() => {
+              SetShowMore(false);
+            }}
+          >
+            ver menos
+          </button>
+        </>
+      )}
+      {!showMore && limitString(description).addButton && (
+        <button
+          type="button"
+          onClick={() => {
+            SetShowMore(true);
+          }}
+        >
+          Ver mas
+        </button>
+      )}
     </div>
   );
 };
