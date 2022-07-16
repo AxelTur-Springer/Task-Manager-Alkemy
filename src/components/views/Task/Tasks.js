ba1c1c9e310e55a007cd9147a7e1b7249fd57d01
AxelTur-Springer <axel.turspringer@gmail.com>
2022-07-16 16:45:08 -0300

    adding skeleton to all tasks when first refresh or entrance starts

--- src/components/views/Task/Tasks.js
+++ src/components/views/Task/Tasks.js
@@ -26,6 +26,7 @@ export const Tasks = () => {
   const [renderList, setRenderList] = useState(null);
   const [tasksFromWho, setTasksFromWho] = useState("ALL");
   const [searchTerm, setSearchTerm] = useState("");
+  const [initialLoad, setInitialLoad] = useState(true);
   const { isPhone } = useResize();
   const dispatch = useDispatch();
 
@@ -57,6 +58,10 @@ export const Tasks = () => {
           })
         )
     );
+    if (tasks.length > 0) {
+      setInitialLoad(false);
+    } else {
+    }
   }, [tasks]);
   //managing search
   useEffect(() => {
@@ -164,33 +169,38 @@ export const Tasks = () => {
               <option value="HIGH">Alta</option>
             </select>
           </div>
-          {isPhone ? (
-            !renderList?.length ? (
-              <div>No hay tareas creadas</div>
+
+          {!initialLoad ? (
+            isPhone ? (
+              !renderList?.length ? (
+                <div>No hay tareas creadas</div>
+              ) : (
+                <div className="list phone">{renderAllCards()}</div>
+              )
             ) : (
-              <div className="list phone">{renderAllCards()}</div>
+              <div className="list_group">
+                {!renderList?.length ? (
+                  <div> No hay tareas creadas</div>
+                ) : (
+                  <>
+                    <div className="list">
+                      <h4>Nuevas</h4>
+                      {renderColumnCards("NEW")}
+                    </div>
+                    <div className="list">
+                      <h4>En proceso</h4>
+                      {renderColumnCards("IN PROGRESS")}
+                    </div>
+                    <div className="list">
+                      <h4>Finalizadas</h4>
+                      {renderColumnCards("FINISHED")}
+                    </div>
+                  </>
+                )}
+              </div>
             )
           ) : (
-            <div className="list_group">
-              {!renderList?.length ? (
-                <div> No hay tareas creadas</div>
-              ) : (
-                <>
-                  <div className="list">
-                    <h4>Nuevas</h4>
-                    {renderColumnCards("NEW")}
-                  </div>
-                  <div className="list">
-                    <h4>En proceso</h4>
-                    {renderColumnCards("IN PROGRESS")}
-                  </div>
-                  <div className="list">
-                    <h4>Finalizadas</h4>
-                    {renderColumnCards("FINISHED")}
-                  </div>
-                </>
-              )}
-            </div>
+            <Skeleton height={500} />
           )}
         </section>
       </main>
