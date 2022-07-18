5455ef86ee383cad6a50adc3dcc055e3dcb3348e
AxelTur-Springer <axel.turspringer@gmail.com>
2022-07-18 11:57:17 -0300

    title

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store/store.js";
 import { App } from "./App";
@@ -9,10 +9,10 @@ import { App } from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="skillUpAlkemyReact-II/">
+    <HashRouter basename="skillUpAlkemyReact-II/">
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
